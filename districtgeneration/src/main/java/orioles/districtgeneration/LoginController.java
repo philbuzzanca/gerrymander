e4102ef16b645b0ef6c3b8bacb4ed1e39620abc7
Antonio Aliberti <antonio.aliberti@stonybrook.edu>
2018-04-08 22:34:23 -0400

    Add LoginController skeleton

---
+++ districtgeneration/src/main/java/orioles/districtgeneration/LoginController.java
@@ -0,0 +1,27 @@
+package orioles.districtgeneration;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @PostMapping("/login")
+    public User login(HttpSession session){
+        User user = new User("sampleuser");
+        session.setAttribute("user", user);
+        return user;
+    }
+    
+    @RequestMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "OK";
+    }
+    
+    @PostMapping("/register")
+    public String register(){
+        return "OK";
+    }
+}
