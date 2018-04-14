6a1ba21f0ccb2ec976e78ceb917efe3bf6f7e1e2
Zachary Koch <zachary.koch@stonybrook.edu>
2018-04-14 15:21:14 -0400

    Added Compactness class

---
+++ districtgeneration/src/main/java/orioles/districtgeneration/Compactness.java
@@ -0,0 +1,18 @@
+package orioles.districtgeneration;
+
+public class Compactness implements Measure{
+
+    
+    public double calculateGoodness(State state) {
+        return 0;
+    }
+    
+    public double calculateGoodness(CongressionalDistrict district) {
+        return 0;
+    }
+
+    public double normalize(double measure) {
+        return 0;
+    }
+    
+}
