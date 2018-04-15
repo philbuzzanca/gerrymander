24426f81a1befa982e63637f52d26d10e7f92420
Phil <philip.buzzanca@stonybrook.edu>
2018-04-15 12:44:43 -0400

    login checks and sets httpsession, reformatted some files

--- districtgeneration/src/main/java/orioles/districtgeneration/LoginController.java
+++ districtgeneration/src/main/java/orioles/districtgeneration/LoginController.java
@@ -11,29 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    @Autowired
-    private UserRepository userRepository;
-    
-    @PostMapping("/login")
-    public User login(@RequestParam String email, @RequestParam String password){
-        List<User> users = userRepository.findByEmail(email);
-        if(!users.isEmpty()){
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            User user = users.get(0);
-            if(encoder.matches(password, user.getPassword())){
-                System.out.println("logged in");
-                return user;
-            }
-            System.out.println("no log in: passwords not mached");
-            return null;
-        }
-        System.out.println("no log in: email not found");
-        return null;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private HttpSession httpSession;
+  
+  @PostMapping("/login")
+  public User login(@RequestParam String email, @RequestParam String password){
+    if(httpSession.getAttribute("user") != null){
+      return null;
     }
-    
-    @RequestMapping("/logout")
-    public String logout(HttpSession session){
-        session.invalidate();
-        return "OK";
+    List<User> users = userRepository.findByEmail(email);
+    if(!users.isEmpty()){
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      User user = users.get(0);
+      if(encoder.matches(password, user.getPassword())){
+        httpSession.setAttribute("user", user);
+        return user;
+      }
+      return null;
     }
+    return null;
+  }
+  
+  @RequestMapping("/logout")
+  public String logout(){
+    httpSession.invalidate();
+    return "OK";
+  }
 }
