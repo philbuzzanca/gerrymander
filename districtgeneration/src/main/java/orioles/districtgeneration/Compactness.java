b57ed7e60e14fd11b404f08fb8efd29461ca1477
Andrew <andrew.lam@stonybrook.edu>
2018-04-15 21:09:18 -0400

    reduced Compactness a bit

--- districtgeneration/src/main/java/orioles/districtgeneration/Compactness.java
+++ districtgeneration/src/main/java/orioles/districtgeneration/Compactness.java
@@ -3,30 +3,20 @@ package orioles.districtgeneration;
 import java.util.List;
 
 public class Compactness implements Measure{
-
-    
     public double calculateGoodness(State state) {
         List<CongressionalDistrict> districts = state.getCongressionalDistricts();
-        double[] districtGoodness = new double[state.getCongressionalDistricts().size()];
-        double sumGoodness = 0;
-        for(int i=0; i<districtGoodness.length; i++){
-            sumGoodness+=calculateGoodness(districts.get(i));
-        }
-        sumGoodness = sumGoodness/districts.size();
-        
-        return sumGoodness;
+        double sumGoodness = districts.stream().mapToDouble(this::calculateGoodness).sum();
+        return sumGoodness/districts.size();
     }
     
     public double calculateGoodness(CongressionalDistrict district) {
-        
         double area = district.calculateArea();
         double perimeter = district.calculatePerimeter();
         
         double r = Math.sqrt(area/Math.PI);
         double equalAreaPerimeter = 2 * Math.PI * r;
-        double score = 1 / (perimeter/equalAreaPerimeter);
-        
-        return score;
+
+		return equalAreaPerimeter / perimeter;
     }
 
     public double normalize(double measure) {
