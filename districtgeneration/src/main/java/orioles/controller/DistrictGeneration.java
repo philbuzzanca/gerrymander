2c6bece07a3f5b7f7e0f1c2272e87446a3421abb
Andrew <andrew.lam@stonybrook.edu>
2018-04-16 00:33:36 -0400

    UserRepo added, but broke all controllers?

--- districtgeneration/src/main/java/orioles/controller/DistrictGeneration.java
+++ districtgeneration/src/main/java/orioles/controller/DistrictGeneration.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("orioles.persistence")
-public class DistrictGenerationApplication {
+@ComponentScan({"orioles.persistence"})
+public class DistrictGeneration {
 	public static void main(String[] args) {
-		SpringApplication.run(DistrictGenerationApplication.class, args);
+		SpringApplication.run(DistrictGeneration.class, args);
 	}
 }
