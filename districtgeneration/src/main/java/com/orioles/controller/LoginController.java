0e7ceca2047cbc631599905939609ccb721a97aa
Phil <philip.buzzanca@stonybrook.edu>
2018-04-17 19:37:55 -0400

    Moved password functions to PasswordUtility. Moved update account method to new AccountController class

--- districtgeneration/src/main/java/com/orioles/controller/LoginController.java
+++ districtgeneration/src/main/java/com/orioles/controller/LoginController.java
@@ -1,12 +1,11 @@
 package com.orioles.controller;
 
-import com.orioles.constants.Party;
+import com.orioles.Security.PasswordUtility;
 import com.orioles.model.User;
 import com.orioles.persistence.UserRepository;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,21 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
   @Autowired
   private UserRepository userRepository;
   @Autowired
   private HttpSession httpSession;
-  
+
   @PostMapping("/login")
-  public User login(@RequestParam String email, @RequestParam String password){
-    if(httpSession.getAttribute("user") != null){
+  public User login(@RequestParam String email, @RequestParam String password) {
+    if (httpSession.getAttribute("user") != null) {
       return null;
     }
     List<User> users = userRepository.findByEmailIgnoreCase(email);
-    if(!users.isEmpty()){
-      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    if (!users.isEmpty()) {
       User user = users.get(0);
-      if(encoder.matches(password, user.getPassword())){
+      if (PasswordUtility.matches(password, user.getPassword())) {
         httpSession.setAttribute("user", user);
         return user;
       }
@@ -36,44 +35,10 @@ public class LoginController {
     }
     return null;
   }
-  
+
   @RequestMapping("/logout")
-  public String logout(){
+  public String logout() {
     httpSession.invalidate();
-    return "OK";
-  }
-  
-  @PostMapping("/update")
-  public String update(@RequestParam String newUsername, @RequestParam String newPassword,
-          @RequestParam String newParty){
-    User user = (User) httpSession.getAttribute("user");
-    if(!(newUsername = newUsername.trim()).equals("")){
-      user.setUsername(newUsername);
-    }
-    if(!(newPassword).equals("")){
-      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-      user.setPassword(encoder.encode(newPassword));
-    }
-    switch(newParty){
-      case "":
-        break;
-      case "0":
-        user.setParty(Party.REPUBLICAN);
-        break;
-      case "1":
-        user.setParty(Party.DEMOCRAT);
-        break;
-      case "2":
-        user.setParty(Party.GREEN);
-        break;
-      case "3":
-        user.setParty(Party.LIBERTARIAN);
-        break;
-      default:
-        user.setParty(Party.OTHER);
-        break;
-    }
-    userRepository.save(user);
     return "OK";
   }
 }
