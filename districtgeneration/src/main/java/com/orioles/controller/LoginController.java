fd17abc84eb2256d091945ff1d9400f66994998d
Phil <philip.buzzanca@stonybrook.edu>
2018-04-17 18:28:57 -0400

    functional registration/login/account update in new file structure

--- districtgeneration/src/main/java/com/orioles/controller/LoginController.java
+++ districtgeneration/src/main/java/com/orioles/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.orioles.controller;
 
+import com.orioles.constants.Party;
+import com.orioles.model.User;
+import com.orioles.persistence.UserRepository;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.orioles.model.User;
-import com.orioles.persistence.UserRepository;
 
 @RestController
 public class LoginController {
@@ -20,21 +21,18 @@ public class LoginController {
   
   @PostMapping("/login")
   public User login(@RequestParam String email, @RequestParam String password){
-    User user = (User) httpSession.getAttribute("user");
-    if(user != null){
-      return user;
-    }
-
-    List<User> users = userRepository.findByEmail(email);
-    if(users.isEmpty()) {
+    if(httpSession.getAttribute("user") != null){
       return null;
     }
-
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    user = users.get(0);
-    if(encoder.matches(password, user.getPassword())){
-      httpSession.setAttribute("user", user);
-      return user;
+    List<User> users = userRepository.findByEmailIgnoreCase(email);
+    if(!users.isEmpty()){
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      User user = users.get(0);
+      if(encoder.matches(password, user.getPassword())){
+        httpSession.setAttribute("user", user);
+        return user;
+      }
+      return null;
     }
     return null;
   }
@@ -42,6 +40,40 @@ public class LoginController {
   @RequestMapping("/logout")
   public String logout(){
     httpSession.invalidate();
+    return "OK";
+  }
+  
+  @PostMapping("/update")
+  public String update(@RequestParam String newUsername, @RequestParam String newPassword,
+          @RequestParam String newParty){
+    User user = (User) httpSession.getAttribute("user");
+    if(!(newUsername = newUsername.trim()).equals("")){
+      user.setUsername(newUsername);
+    }
+    if(!(newPassword).equals("")){
+      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+      user.setPassword(encoder.encode(newPassword));
+    }
+    switch(newParty){
+      case "":
+        break;
+      case "0":
+        user.setParty(Party.REPUBLICAN);
+        break;
+      case "1":
+        user.setParty(Party.DEMOCRAT);
+        break;
+      case "2":
+        user.setParty(Party.GREEN);
+        break;
+      case "3":
+        user.setParty(Party.LIBERTARIAN);
+        break;
+      default:
+        user.setParty(Party.OTHER);
+        break;
+    }
+    userRepository.save(user);
     return "OK";
   }
 }
--- districtgeneration/src/main/java/com/orioles/controller/RegisterController.java
+++ districtgeneration/src/main/java/com/orioles/controller/RegisterController.java
@@ -1,11 +1,14 @@
 package com.orioles.controller;
 
+import com.orioles.constants.Party;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.orioles.persistence.UserRepository;
 import com.orioles.model.User;
+import java.util.List;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
 public class RegisterController {
@@ -14,10 +17,20 @@ public class RegisterController {
   
   @PostMapping("/register")
   public String register(@RequestParam String username, @RequestParam String email, @RequestParam String password){
-    if(!userRepository.findByEmail(email).isEmpty()){
+    if((username = username.trim()).equals("")){
       return null;
     }
-    userRepository.save(new User(username, email, password));
+    List<User> users = userRepository.findByEmailIgnoreCase(email);
+    if(!users.isEmpty()){
+      return null;
+    }
+    User user = new User();
+    user.setUsername(username);
+    user.setEmail(email);
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    user.setPassword(encoder.encode(password));
+    user.setParty(Party.OTHER);
+    userRepository.save(user);
     return "OK";
   }
 }
