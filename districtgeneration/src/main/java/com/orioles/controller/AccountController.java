c9b01f874cece47d43f65ddca5ed75ee7dbeb405
Andrew <andrew.lam@stonybrook.edu>
2018-04-17 20:43:23 -0400

    cleaned up /update

--- districtgeneration/src/main/java/com/orioles/controller/AccountController.java
+++ districtgeneration/src/main/java/com/orioles/controller/AccountController.java
@@ -4,49 +4,41 @@ import com.orioles.Security.PasswordUtility;
 import com.orioles.constants.Party;
 import com.orioles.model.User;
 import com.orioles.persistence.UserRepository;
+
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
-  
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private HttpSession httpSession;
-  
-  @PostMapping("/update")
-  public String update(@RequestParam String newUsername, @RequestParam String newPassword, @RequestParam String newParty) {
-    User user = (User) httpSession.getAttribute("user");
-    if (!(newUsername = newUsername.trim()).equals("")) {
-      user.setUsername(newUsername);
-    }
-    if (!(newPassword).equals("")) {
-      user.setPassword(PasswordUtility.encode(newPassword));
-    }
-    switch (newParty) {
-      case "":
-        break;
-      case "0":
-        user.setParty(Party.REPUBLICAN);
-        break;
-      case "1":
-        user.setParty(Party.DEMOCRAT);
-        break;
-      case "2":
-        user.setParty(Party.GREEN);
-        break;
-      case "3":
-        user.setParty(Party.LIBERTARIAN);
-        break;
-      default:
-        user.setParty(Party.OTHER);
-        break;
-    }
-    userRepository.save(user);
-    return "OK";
-  }
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private HttpSession httpSession;
+
+	@PostMapping("/update")
+	public String update(@RequestParam String newUsername,
+						 @RequestParam String newPassword,
+						 @RequestParam String newParty) {
+		User user = (User) httpSession.getAttribute("user");
+		if (!(newUsername = newUsername.trim()).equals("")) {
+			user.setUsername(newUsername);
+		}
+		if (!(newPassword).equals("")) {
+			user.setPassword(PasswordUtility.encode(newPassword));
+		}
+		if (!newParty.equals("")) {
+			int partyNum = Integer.parseInt(newParty);
+			if (partyNum >= 0 && partyNum <= Party.values().length) {
+				user.setParty(Party.values()[partyNum]);
+			}
+		}
+
+		userRepository.save(user);
+		return "OK";
+	}
 }
--- districtgeneration/src/main/java/com/orioles/model/User.java
+++ districtgeneration/src/main/java/com/orioles/model/User.java
@@ -7,67 +7,57 @@ import javax.persistence.*;
 
 @Entity
 public class User {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
-  private String username;
-  private String password;
-  private String email;
-  
-  //@Column(name="is_activated")
-  //private boolean isActivated;
-  private Party party;
-  
-  public User() {}
-  
-  public User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.party = Party.OTHER;
-    
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    this.password = encoder.encode(password);
-  }
-  
-  public String getUsername() {
-    return username;
-  }
-  
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  
-  public String getPassword() {
-    return password;
-  }
-  
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  
-  public String getEmail() {
-    return email;
-  }
-  
-  public void setEmail(String email) {
-    this.email = email;
-  }
-  
-  /*public boolean isIsActivated() {
-  return isActivated;
-  }*/
-  
-  /*public void setIsActivated(boolean isActivated) {
-  this.isActivated = isActivated;
-  }*/
-  
-  public Party getParty() {
-    return party;
-  }
-  
-  public void setParty(Party party) {
-    this.party = party;
-  }
-  
-  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private Party party;
+
+	public User() {
+	}
+
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.party = Party.OTHER;
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		this.password = encoder.encode(password);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Party getParty() {
+		return party;
+	}
+
+	public void setParty(Party party) {
+		this.party = party;
+	}
+
+
 }
