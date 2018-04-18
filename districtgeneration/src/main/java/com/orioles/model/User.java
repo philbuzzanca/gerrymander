adff9fe16f797908e0304d3f338c8f7f11d349dc
Phil Buzzanca <philip.buzzanca@stonybrook.edu>
2018-04-17 22:27:58 -0400

    changed user to match new db

--- districtgeneration/src/main/java/com/orioles/model/User.java
+++ districtgeneration/src/main/java/com/orioles/model/User.java
@@ -7,43 +7,41 @@ import javax.persistence.*;
 
 @Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String username;
-	private String password;
-	private Party party;
-
-	public User() {}
-
-	public User(String username, String password) {
-		this.username = username;
-		this.party = Party.OTHER;
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		this.password = encoder.encode(password);
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Party getParty() {
-		return party;
-	}
-
-	public void setParty(Party party) {
-		this.party = party;
-	}
+    @Id
+    private String username;
+    private String password;
+    private Party party;
+    
+    public User() {}
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.party = Party.OTHER;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        this.password = encoder.encode(password);
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Party getParty() {
+        return party;
+    }
+    
+    public void setParty(Party party) {
+        this.party = party;
+    }
 }
