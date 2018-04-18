df0ea6053dc4d9d6a7d027c0af7b3b038bbba9e3
Andrew <andrew.lam@stonybrook.edu>
2018-04-17 20:43:23 -0400

    cleaned up /update

--- districtgeneration/src/main/java/com/orioles/Security/PasswordUtility.java
+++ districtgeneration/src/main/java/com/orioles/Security/PasswordUtility.java
@@ -1,23 +1,16 @@
 package com.orioles.Security;
 
-import com.orioles.constants.Constants;
-
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtility {
 
-  public static String encode(String rawPassword) {
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    return encoder.encode(rawPassword);
-  }
+	public static String encode(String rawPassword) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(rawPassword);
+	}
 
-  public static boolean matches(String rawPassword, String encodedPassword) {
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    return encoder.matches(rawPassword, encodedPassword);
-  }
+	public static boolean matches(String rawPassword, String encodedPassword) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.matches(rawPassword, encodedPassword);
+	}
 }
--- districtgeneration/src/main/java/com/orioles/controller/LoginController.java
+++ districtgeneration/src/main/java/com/orioles/controller/LoginController.java
@@ -3,8 +3,10 @@ package com.orioles.controller;
 import com.orioles.Security.PasswordUtility;
 import com.orioles.model.User;
 import com.orioles.persistence.UserRepository;
+
 import java.util.List;
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,32 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private HttpSession httpSession;
 
-  @Autowired
-  private UserRepository userRepository;
-  @Autowired
-  private HttpSession httpSession;
+	@PostMapping("/login")
+	public User login(@RequestParam String email, @RequestParam String password) {
+		if (httpSession.getAttribute("user") != null) {
+			return null;
+		}
+		List<User> users = userRepository.findByEmailIgnoreCase(email);
+		if (!users.isEmpty()) {
+			User user = users.get(0);
+			if (PasswordUtility.matches(password, user.getPassword())) {
+				httpSession.setAttribute("user", user);
+				return user;
+			}
+			return null;
+		}
+		return null;
+	}
 
-  @PostMapping("/login")
-  public User login(@RequestParam String email, @RequestParam String password) {
-    if (httpSession.getAttribute("user") != null) {
-      return null;
-    }
-    List<User> users = userRepository.findByEmailIgnoreCase(email);
-    if (!users.isEmpty()) {
-      User user = users.get(0);
-      if (PasswordUtility.matches(password, user.getPassword())) {
-        httpSession.setAttribute("user", user);
-        return user;
-      }
-      return null;
-    }
-    return null;
-  }
-
-  @RequestMapping("/logout")
-  public String logout() {
-    httpSession.invalidate();
-    return "OK";
-  }
+	@RequestMapping("/logout")
+	public String logout() {
+		httpSession.invalidate();
+		return "OK";
+	}
 }
