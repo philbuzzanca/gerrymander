fd7c4e1804b2d5f2ae1bccdfa75f0dbb7bdc13cb
Andrew <andrew.lam@stonybrook.edu>
2018-04-29 14:58:28 -0400

    prepare to merge

--- districtgeneration/src/main/java/com/orioles/persistence/PrecinctRepository.java
+++ districtgeneration/src/main/java/com/orioles/persistence/PrecinctRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PrecinctRepository extends CrudRepository<Precinct, PrecinctId> {
     List<Precinct> findByIdState(String stateName);
+//    List<Precinct> findById(int id);
 }
