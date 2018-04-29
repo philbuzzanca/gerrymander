b009d2458787e0172bd4612b982548d28514a11c
Zachary Koch <zachary.koch@stonybrook.edu>
2018-04-29 14:44:10 -0400

    Added Demographic and Population goodness calculations. Removed Demographic class because its the same as Race.

--- districtgeneration/src/main/java/com/orioles/model/Stats.java
+++ districtgeneration/src/main/java/com/orioles/model/Stats.java
@@ -1,6 +1,5 @@
 package com.orioles.model;
 
-import com.orioles.constants.Demographic;
 import com.orioles.constants.Party;
 import com.orioles.constants.Race;
 import java.util.Map;
@@ -8,7 +7,6 @@ import java.util.Map;
 public class Stats {
     private Map<Race, Long> races;
     private Map<Party, Long> parties;
-    private Map<Demographic, Long> demographics;
     private long population;
 
     
@@ -16,14 +14,13 @@ public class Stats {
         
     }
     
-	Stats(Map<Race, Long> races, Map<Party, Long> parties, long population, Map<Demographic, Long> demographics) {
+	Stats(Map<Race, Long> races, Map<Party, Long> parties, long population) {
 		this.races = races;
 		this.parties = parties;
-                this.demographics = demographics;
 		this.population = population;
 	}
 
-	Map<Race, Long> getRaces() {
+    public Map<Race, Long> getRaces() {
         return races;
     }
 
@@ -47,27 +44,15 @@ public class Stats {
         this.population = population;
     }
     
-    public Map<Demographic, Long> getDemographics() {
-        return demographics;
-    }
-
-    void setDemographics(Map<Demographic, Long> demographics) {
-        this.demographics = demographics;
-    }
-    
     
 
-	static void summarize(Map<Race, Long> races, Map<Party, Long> parties, Stats eachStat, Stats overallStats,Map<Demographic, Long> demographics) {
+	static void summarize(Map<Race, Long> races, Map<Party, Long> parties, Stats eachStat, Stats overallStats) {
 		for (Race r : Race.values()) {
 			races.put(r, races.get(r) + eachStat.getRaces().get(r));
 		}
 
 		for (Party p : Party.values()) {
 			parties.put(p, parties.get(p) + eachStat.getParties().get(p));
-		}
-                
-                for (Demographic d : Demographic.values()) {
-			demographics.put(d, demographics.get(d) + eachStat.getDemographics().get(d));
 		}
 
 		overallStats.setPopulation(overallStats.getPopulation() + eachStat.getPopulation());
